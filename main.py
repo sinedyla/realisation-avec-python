a799f59bdc5c732cd8e572c4fb885972bfc9f109
Fara Ka <sinedyla@gmail.com>
2026-04-03 23:53:07 +0000

    FENERATEUR DE QR CODE

---
+++ main.py
@@ -0,0 +1,13 @@
+import qrcode
+
+lien = input("Veuillez entrer le lien : ").strip()
+
+chemin = "C:\\Users\\sined\\OneDrive - Université Cheikh Anta DIOP de DAKAR\\Bureau\\Mes Projets\\QR Code\\votre_qrcode.png"
+
+qr = qrcode.QRCode()
+qr.add_data(lien)
+
+image = qr.make_image()
+image.save(chemin)
+
+print("Votre QR Code a été generé :) ")
